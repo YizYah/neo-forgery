dcf23f91d553fb5ab87fc47023e2531af16e517f
YizYah <yyakovson613@gmail.com>
2021-06-10 15:03:39 +0300

    refactor: mostly wrote tests, but changed getSession a bit

--- test/custom/mockSessionFromFunction.test.ts
+++ test/custom/mockSessionFromFunction.test.ts
@@ -1,13 +1,13 @@
 import test from 'ava'
 const mockSessionFromFunction = require("../../src/custom/session/mockSessionFromFunction")
 import {sampleRecordList} from "./data/sampleRecordList";
-import {mockResultsFromData} from "../../../../export-server-all/export-server/test/custom/neoforgery/results/mockResultsFromData";
+import {mockResultsFromData} from "../../src/custom/results/mockResultsFromData";
 
 const sessionRunMock = (query: string, params: any) => {
     return mockResultsFromData(sampleRecordList);
 };
 
-test('mockSession', t => {
+test('mockSessionFromFunction', t => {
     const session = mockSessionFromFunction(sessionRunMock)
     t.is(session.run, sessionRunMock)
     t.not(session.run, ()=>{return 1})
